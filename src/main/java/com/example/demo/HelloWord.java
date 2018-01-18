c8f8f67469baa47bc77a8e41082e75dae145f0ed
Rahul.Singh <rahulsinghc606@gmail.com>
2018-01-18 11:37:12 +0530

    First Commit

---
+++ src/main/java/com/example/demo/HelloWord.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWord {
+@RequestMapping("/test")
+public String getMessage()
+{
+return "WELCOME TO MICROSERVICE";	
+}
+
+@RequestMapping("/detail")
+public String getDetail()
+{
+return "HII I AM SPRING BOOT ";	
+}
+
+}
